7781ab5bb93d5b14ee29278f6c4cc1a86a68df97
Aditya Salgude <p.adi.salgude@gmail.com>
2022-07-15 13:18:21 +0530

    changes Api key

--- src/app/config.ts
+++ src/app/config.ts
@@ -3,5 +3,5 @@ export default {
     TESLA_ARTICLE_URL: 'https://newsapi.org/v2/everything?q=tesla&from=2022-06-15&sortBy=publishedAt',
     BUSINESS_ARTICLE_URL: 'https://newsapi.org/v2/top-headlines?country=us&category=business',
     TECHCRUNCH_ARTICLE_URL: 'https://newsapi.org/v2/top-headlines?sources=techcrunch',
-    API_KEY: 'd67eda09cf3c497f897a997560e71c59'
+    API_KEY: 'e8269f3768144466958a06225121ee6c'
 };
